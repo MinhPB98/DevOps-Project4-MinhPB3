390fb5131d082229c72061334f62738eacb937d8
MinhPB98 <pbminheeeiu16068@gmail.com>
2022-07-11 14:21:23 +0700

    add Dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build . -t project-4-minhpb3:latest
 # Step 2: 
 # List docker images
-
+docker images
 # Step 3: 
 # Run flask app
+docker run --name project4-udacity --mount source=volume,target=/app/output_txt_files -p 8080:80 project-4-minhpb3:latest
